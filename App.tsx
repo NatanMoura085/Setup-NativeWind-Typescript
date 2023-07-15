92a4172b45d1a104dc47a6e0872a4faaaaa53efe
Natan <mouranatn933@gmail.com>
2023-07-15 04:40:01 -0300

    setup

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-zinc-900">
+      <Text className="text-white text-4xl font-bold">Hello Nathan!</Text>
+    </View>
+  );
+}
